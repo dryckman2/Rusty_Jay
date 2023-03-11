d8296779ead7d5503e3766edc51703ac55d23844
dryckman2 <dryckman2@huskers.unl.edu>
2023-03-11 12:25:38 -0600

    File Structure Fix

--- target/debug/build/libc-da11048515868f5e/build_script_build-da11048515868f5e.d
+++
@@ -1,5 +0,0 @@
-/Users/davidryckman/CLionProjects/Rusty_Jay/target/debug/build/libc-da11048515868f5e/build_script_build-da11048515868f5e: /Users/davidryckman/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.139/build.rs
-
-/Users/davidryckman/CLionProjects/Rusty_Jay/target/debug/build/libc-da11048515868f5e/build_script_build-da11048515868f5e.d: /Users/davidryckman/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.139/build.rs
-
-/Users/davidryckman/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.139/build.rs:
--- target/debug/deps/rusty_jay_patel-a63430aa36551fc9.d
+++
@@ -1,6 +0,0 @@
-/Users/davidryckman/CLionProjects/Rusty_Jay/target/debug/deps/rusty_jay_patel-a63430aa36551fc9: src/main.rs src/jay_patel.rs
-
-/Users/davidryckman/CLionProjects/Rusty_Jay/target/debug/deps/rusty_jay_patel-a63430aa36551fc9.d: src/main.rs src/jay_patel.rs
-
-src/main.rs:
-src/jay_patel.rs:
